8cdcef045230c6a9c72966bff188fbb36b69cd00
mb1te <45900239+mb1te@users.noreply.github.com>
2019-09-13 20:45:41 +0300

    upd

--- bot.py
+++ bot.py
@@ -1,9 +1,10 @@
 import requests
+import time
 import vk_api
 from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
 
 def send_to_telegram(event):
-	data = {'chat_id' : '@ortoblogge', 'text' : event.object.text} #id канала
+	data = {'chat_id' : '@ortoblogge', 'text' : event} #id канала
 	url = 'https://api.telegram.org/bot745619680:AAFlpDDvR8eEUP3hg2H-VHy9I-c1XlU72NU/sendMessage'
 	requests.get(url, params=data)
 
@@ -11,6 +12,24 @@ vk_session = vk_api.VkApi(token='bf9c9e8265613a6cc6b49352f4f5586433dcc487bf17d15
 vk = vk_session.get_api()
 longpoll = VkBotLongPoll(vk_session, '150420788') #id группы
 
-for event in longpoll.listen():
-	if event.type == VkBotEventType.WALL_POST_NEW and event.object['from_id'] == -150420788:
-		send_to_telegram(event)
+
+while True:
+    try:
+        for event in longpoll.listen():
+                if event.type == VkBotEventType.WALL_POST_NEW and event.object['from_id'] == -150420788:
+                    print("successful", time.ctime())
+                    if len(event.object.text) > 4096:
+                        t = event.object.text.split('\n')
+                        msg = ''
+                        for i in t:
+                            if len(msg) + len(i) + 1 < 4096:
+                                msg = msg + i + "\n"
+                            else:
+                                send_to_telegram(msg)
+                                msg = i + "\n"
+                        send_to_telegram(msg)
+                    else:
+                        send_to_telegram(event.object.text)
+
+    except:
+        print("failed", time.ctime())
